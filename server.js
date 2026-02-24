91448465a2735da81600511b1905356d48b6a650
adil-tounly <ad.tounly@gmail.com>
2026-02-24 16:07:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -39,5 +39,8 @@ app.post("/send", async (req, res) => {
     res.send("Message sent")
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log("Server running"))
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log("Server running on port " + PORT);
+});
